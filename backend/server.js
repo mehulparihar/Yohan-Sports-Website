acc605c13866d7952e956d276a8c0b8a17f020d6
Anonymous <anonymous@Anonymous.Anonymous>
2025-11-23 22:21:56 +0530

    client url

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.CLIENT_URL, 
   credentials: true,             
 }));
 
